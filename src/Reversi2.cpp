12c5702775604b14d695fdb2158b06602c70f85d
LiamAkkerman <liam.akkerman@gmail.com>
2020-04-01 22:47:09 -0700

    fix actions() for real rules

--- src/Reversi2.cpp
+++ src/Reversi2.cpp
@@ -24,7 +24,7 @@ void Reversi2::print() {
 void Reversi2::print(const Node& node) {
     std::cout << "┌───┬───┬───┬───┬───┬───┬───┬───┐" << std::endl;
     //std::cout << "| " << char(head->board[0]) << " ";
-    for (auto i = 0; i < node.board.size()-1; i++) {
+    for (uint8_t i = 0; i < node.board.size()-1; i++) {
         std::cout << "| " << char(node.board[i]) << " ";
         if ((i+1) % 8 == 0) {
             std::cout << "|" << std::endl << "├───┼───┼───┼───┼───┼───┼───┼───┤" << std::endl;
@@ -34,15 +34,19 @@ void Reversi2::print(const Node& node) {
     std::cout << "└───┴───┴───┴───┴───┴───┴───┴───┘" << std::endl;
 }
 
+//checks that the x doesn't change too much, indicating a wrap-around. and within the bounds
+bool Reversi2::consistent_line(int8_t prev_point, int8_t curr_point) {
+    return ((curr_point > 0 || curr_point <= 64) && (abs((curr_point % 8) - (prev_point % 8)) <= 1));
+}
 
 std::vector<Reversi2::action_t> Reversi2::actions() {
     return actions(*head);
 }
 
 std::vector<Reversi2::action_t> Reversi2::actions(const Node& node) {
-    std::vector<action_t> moves{};
-    std::vector<int8_t> check{};
+    auto moves =std::vector<action_t>{};
 
+    auto theta_list = std::array<int8_t, 8>{-9, -8, -7, -1, 9, 8, 7, 1};
     space_t player, opponent;
     if (turn % 2 == 0) {
         player = player1;
@@ -51,54 +55,39 @@ std::vector<Reversi2::action_t> Reversi2::actions(const Node& node) {
         player = player2;
         opponent = player1;
     }
-
-    for (auto i = 0; i < node.board.size(); i++) { //it'd be nice if this was range based
-        if (node.board[i] == player) { 
-            // TODO there has got to be a better way to do this. It's so many lines for so little.
-            if                  (i / 8 >= 2) { //N
-                check.push_back(-8);
-            }
-            if ((i % 8 <= 5) && (i / 8 >= 2)) { //NE
-                check.push_back(-7);
-            }
-            if (i % 8 <= 5) {                   //E
-                check.push_back(1);
-            }
-            if ((i % 8 <= 5) && (i / 8 <= 5)) { //SE
-                check.push_back(9);
-            }
-            if                  (i / 8 <= 5) {  //S
-                check.push_back(8);
-            }
-            if ((i % 8 >= 2) && (i / 8 <= 5)) { //SW
-                check.push_back(7);
-            }
-            if (i % 8 >= 2) {                   //W
-                check.push_back(-1);
-            }
-            if ((i % 8 >= 2) && (i / 8 >= 2)) { //NW
-                check.push_back(-9);
-            }
-
-            for (auto nieghbor : check) {
-                if ((node.board[i + nieghbor] == opponent) && (node.board[i + 2*nieghbor] == blank)) { 
-                    //std::cout << "found move: " << i << " to " << (i+2*nieghbor) << std::endl;
-                    moves.push_back(action_t{i, i+2*nieghbor});
+    
+    // this part works sort of like polar coordinates. it first goes through the entire board to find it's own pieces. 
+    // then it goes through each cardinal direction and moves outward until it:
+    //  - can make a move and then adds it to the moves vector
+    //  - hits the edge of the board
+    //  - sees it's own type of piece
+    // it only moves out along the opponent's type of pieces
+    for (uint8_t i = 0; i < node.board.size(); i++) { //it'd be nice if this was range based
+        if (node.board.at(i) == player) {
+            for (auto theta : theta_list) {
+                for (int8_t r = 1; consistent_line(i + r*theta, i + (r-1)*theta); r++) {
+                    if ((r > 1) && (node.board.at(i + r*theta) == blank)) {
+                        moves.push_back(action_t{i + r*theta, i});
+                        break;
+                    } else if (node.board.at(i + r*theta) != opponent) { 
+                        break;
+                    }
                 }
             }
-
-            check.clear();
         }
     }
 
-    /*
-    -18     -16     -14
+    return moves; //I'm not sure why the linter is picking this up as an error
+
+    /*  
+   -18     -16     -14
         -9  -8  -7
     -2  -1  n   1   2
         7   8   9
     14      16      18
 
     dn = n + 1*dx + 8*dy
+    dn = n + r*theta
 
     x = (n mod 8) + 1
     y = (n / 8) + 1
@@ -106,23 +95,19 @@ std::vector<Reversi2::action_t> Reversi2::actions(const Node& node) {
     (x >= 3) --> (-1)(dx=-1) left
     (x <= 6) --> (+1)(dx= 1) right
     (y >= 3) --> (-8)(dy=-1) above
-    (y <= 6) --> (+8)(dy= 1) below
+    (y <= 6) --> (+8)(dy= 1) below 
     */
-    return moves; //I'm not sure why the linter is picking this up as an error
 }
 
 bool Reversi2::goal_test() { //TODO make player agnostic
     return goal_test(*head);
-
-
-    
-
 }
 
 bool Reversi2::goal_test(const Node& node) {
     return (actions(node).size() == 0);
 }
 
+/* 
 Reversi2::board_t Reversi2::result(const Reversi2::board_t& old_board, Reversi2::action_t action) {
     auto new_board = old_board;
     auto symbol = old_board.at(action.space);
@@ -130,9 +115,9 @@ Reversi2::board_t Reversi2::result(const Reversi2::board_t& old_board, Reversi2:
     new_board.at((action.space+action.destination)/2) = symbol;  // the space that it jumps over
     
     return new_board; //should this be by referance?
-}
+} 
 
-void Reversi2::expand_childred() {
+void Reversi2::expand_children() {
     auto new_boards = std::vector<board_t>{};
     auto moves = actions(*head);
 
@@ -148,3 +133,4 @@ void Reversi2::do_turn(Reversi2::action_t move) {
     turn++;
 }
 
+*/
--- src/Reversi2.h
+++ src/Reversi2.h
@@ -9,7 +9,7 @@
 #include "Node.h"
 
 class Reversi2 {
-    const char blank, player1, player2; 
+    const unsigned char blank, player1, player2; 
     std::unique_ptr<Node> head;
 
     uint8_t turn = 0;
@@ -19,34 +19,32 @@ class Reversi2 {
         typedef uint8_t space_t;
         typedef std::array<space_t, 64> board_t;
         typedef struct {
-            space_t space, destination;
+            int8_t new_space, old_space;
         } action_t;
+        
 
         Reversi2();
 
         void print();
-        
-        
         std::vector<action_t> actions();
+        
         void do_turn(action_t);
         
         bool goal_test(); //TODO make private after tests
 
-        void expand_childred();
+        void expand_children();
+
 
     private:
-        // all of these below are just sort of rough guesses
-        
         std::vector<action_t> actions(const Node&);
+        bool consistent_line(int8_t, int8_t);
+        
         void print(const Node&);
         
+        
         bool goal_test(const Node&);
-        bool valid_move(board_t, space_t, char);
         board_t result(const board_t&, action_t);
-
-        space_t coord2space(std::pair<int,int>);
-        space_t coord2space(int,int);
-
+        
 };
 
 #endif
