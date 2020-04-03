d0141a4aeb59c5c597231876bda6507cf7d5ae64
LiamAkkerman <liam.akkerman@gmail.com>
2020-04-02 22:30:31 -0700

    add function paramater and random choice hueristic

--- src/algorithim.cpp
+++ src/algorithim.cpp
@@ -1,45 +1,61 @@
 #include <ctime>
+#include <functional>
 
 #include "algorithim.h"
 #include "Reversi2.h"
 
-Reversi2::action_t monte_carlo(const Reversi2& game_in, const std::vector<Reversi2::action_t>& choices, Reversi2::action_t (*hueristic)(void)) {
+Reversi2::action_t monte_carlo(const Reversi2& game_in, std::function<Reversi2::action_t(Reversi2&)> hueristic) {
     const uint max_playouts = 1000;
     const uint max_time_ms = 5000;
+    const auto choices = game_in.actions();
     auto wins = std::vector<uint>{};
-    auto best_move = choices.at(0); //TODO this is a placeholder until the best is chosen
     auto player = game_in.whos_turn();
     uint i = 0;
 
     //perfrom posible move then playout
     for (auto choice : choices) {
-        auto game = Reversi2(game_in);
+        auto game = Reversi2(game_in); //beware, does not copy children
         wins.push_back(0);
         game.do_turn(choice);
-        auto next_actions = game.actions();
+        game.expand_children();
 
         for (uint n = 0, curr_time_ms = 0; (n < max_playouts) && (curr_time_ms < max_time_ms); n++){ //TODO make it count time
             while(!game.goal_test()) {
-                next_actions = game.actions();
-                if (next_actions.size() == 0) {
+                game.expand_children();
+                if (game.get_head().children.size() == 0) {
                     game.skip_turn();
                 } else {
-                    auto next_action = hueristic(); //TODO make the h functions and thier param type
+                    auto next_action = hueristic(game);
                     game.do_turn(next_action);
                 }
             }
             if (game.winner() == player) {
                 wins.at(i) += 1;
             }  
-            i++;
-        }  
+        }
+        i++;
     }
-    //TODO choose max scored move
-    return best_move;
+    //choose max wins
+    uint max = 0;
+    uint i_max = 0;
+    for(uint i = 0; i < wins.size(); i++){
+        if(wins[i] > max) {
+            max = wins[i];
+            i_max = i;
+        }
+    }
+    return choices.at(i_max);
 }
 
 
-Reversi2::action_t random_h();
+Reversi2::action_t random_h(Reversi2& game_in) {  
+    auto actions = game_in.actions();
+    auto rand_i = rand() % actions.size();
+
+    return actions.at(rand_i);
+}
+
+
 Reversi2::action_t mobility_h();
 Reversi2::action_t capture_h();
 Reversi2::action_t corner_h();
--- src/algorithim.h
+++ src/algorithim.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <array>
 #include <functional>
+#include <random>
 
 
 
@@ -11,9 +12,9 @@
 #include "Reversi2.h"
 
 
-Reversi2::action_t monte_carlo(const Reversi2&, std::vector<Reversi2::action_t>, Reversi2::action_t (*)(void));
+Reversi2::action_t monte_carlo(const Reversi2&, std::function<Reversi2::action_t(Reversi2&)>);
 
-Reversi2::action_t random_h();
+Reversi2::action_t random_h(Reversi2&);
 Reversi2::action_t mobility_h();
 Reversi2::action_t capture_h();
 Reversi2::action_t corner_h();
